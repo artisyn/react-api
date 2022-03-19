1c2f2495eefa509045c006e5f1fb6ae382dc4c3d
artisyn <artioms1@gmail.com>
2022-03-19 21:00:06 +0200

    v2

--- src/App.js
+++ src/App.js
@@ -5,27 +5,27 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 function App() {
-	const [pokemon, setPokemon] = useState();
-	const [pictureUrl, setPictureUrl] = useState();
-	useEffect(() => {
-		const getPokemons = async () => {
-			try {
-				const response = await axios.get(
-					'https://pokeapi.co/api/v2/pokemon?limit=15'
-				);
-				console.log(response.data.results);
-				setPokemon(response.data.results[0].name);
+	// const [pokemon, setPokemon] = useState();
+	// const [pictureUrl, setPictureUrl] = useState();
+	// useEffect(() => {
+	// 	const getPokemons = async () => {
+	// 		try {
+	// 			const response = await axios.get(
+	// 				'https://pokeapi.co/api/v2/pokemon?limit=15'
+	// 			);
+	// 			console.log(response.data.results);
+	// 			setPokemon(response.data.results[0].name);
 
-				const moreInfo = await axios.get(response.data.results[0].url);
-				const sprite = moreInfo.data.sprites.front_default;
-				console.log(moreInfo);
-				console.log(sprite);
-			} catch (error) {
-				console.error(error);
-			}
-		};
-		getPokemons();
-	}, []);
+	// 			const moreInfo = await axios.get(response.data.results[0].url);
+	// 			const sprite = moreInfo.data.sprites.front_default;
+	// 			console.log(moreInfo);
+	// 			console.log(sprite);
+	// 		} catch (error) {
+	// 			console.error(error);
+	// 		}
+	// 	};
+	// 	getPokemons();
+	// }, []);
 
 	// const DisplayPokemons = (arr) => {
 
--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,20 +1,53 @@
 import React from 'react';
 import './MainSection.scss';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const MainSection = () => {
-	const GetPokemonInfo = () => {};
-	return (
-		<div>
-			<section>
-				<div className="poke__container">
-					<h2>Bulbasaur</h2>
-					<img
-						src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+	const [sectionMarkup, setSectionMarkup] = useState();
+
+	useEffect(() => {
+		const allPokemons = async () => {
+			let markup = '';
+			const response = await axios.get(
+				'https://pokeapi.co/api/v2/pokemon?limit=15'
+			);
+			console.log(response.data.results);
+
+			response.data.results.forEach((element) => {
+				const { url } = element;
+
+				console.log(url);
+				const moreInfo = async () => {
+					const { data } = await axios.get(url);
+					// console.log(data);
+					const { name, sprites } = data;
+
+					console.log(name, sprites.front_default);
+
+					markup += `
+					<div className="poke__container">
+						<h2>${name}</h2>
+							<img
+							src="${sprites.front_default}"
 						alt="pokemon"
 					/>
-				</div>
-			</section>
+					</div>
+				`;
+				};
+				moreInfo();
+			});
+			console.log(markup);
+
+			setSectionMarkup(markup);
+		};
+
+		allPokemons();
+	}, []);
+
+	return (
+		<div>
+			<section>{sectionMarkup}</section>
 		</div>
 	);
 };
